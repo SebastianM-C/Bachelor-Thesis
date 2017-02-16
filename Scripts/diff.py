6028cd7997417ea3cd63f1b930c7c53abc83c5aa
sebastian <m.c.sebastian95@gmail.com>
2017-02-16 21:07:47 +0200

    Add irreductible representation decomposition
    -Use indices instead of variables for easier testing
    -Output formatting improvements
    -Fix histogram bin width

--- Scripts/diff.py
+++ Scripts/diff.py
@@ -5,23 +5,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def statistics(A, B, D, N, file):
+def ir_decomp(E):
+    "Irreductible representations decomposition"
+    reuna = []
+    reuns = []
+    reuns.append(E[0])
+
+    # in order to compute E[j] - E[j-1] using np.diff() we must shift every
+    # element of the array to right with 1 position and ignore diff[0]
+    diff = np.diff(np.roll(E, 1))           # E[j] - E[j-1]
+    E = np.roll(E, 1)
+    E = E[1:]
+    diff1 = np.diff(E)                      # E[j+1] - E[j]
+
+    # print("size of E: ", E.size, "\nsize of diff: ", diff.size)
+    print("diff: ", diff[:20])
+
+    rebde = E[diff <= 0.0005]
+    rebde = rebde[1:]
+    print("\nrebde: ", rebde[:10], "...", rebde[-10:], rebde.size)
+
+    return (rebde, reuna, reuns)
+
+
+def statistics(A, B, D, N, f_index):
     "Compute spacing statistics"
     path = "../Output/B" + str(B) + " N" + str(N)
     os.chdir(path)
     output = open("diff.dat", 'w')
 
-    E = np.loadtxt(file, usecols=(1,), unpack=True)  # Energy levels
+    E = np.loadtxt(input[0], usecols=(1,), unpack=True)  # Energy levels
+
     # Select only stable levels
     E = E[stable_levels]
+
+    if f_index > 0 & f_index < 4:
+        # The energy levels must be decomposed again in
+        # irreductible representations
+        E = ir_decomp(E)[f_index - 1]
+    #    print(ir_rep)
+
     deltaE = np.diff(E)                                  # Energy level spacing
     avgSpacing = (E[-1] - E[0]) / E.size                 # Average spacing
-    print("Statistics parameters: \nA = ", A, "\nB = ", B, "\nD = ",
+    print("\nStatistics parameters: \nA = ", A, "\nB = ", B, "\nD = ",
           D, "\nN = ", N, "\nAverage spacing: ", avgSpacing)
     relSpacing = deltaE / avgSpacing
     relSpacing.tofile(output, sep="\n")
-    # plt.hist(relSpacing, bins=np.arange(0, 4, avgSpacing/2))
-    plt.hist(relSpacing, bins='auto')
+    plt.hist(relSpacing, bins=np.arange(0, 4, 1 / 2))
     plt.show()
     os.chdir("../../Scripts")
     return (deltaE, avgSpacing, relSpacing)
@@ -49,23 +79,30 @@ def difference(A, B, D, N1, N2, file):
     os.chdir("../Scripts")
     return diff
 
+
 # Hamiltonian parameters
 A = 1
-B = [0.1 * A, 0.5 * A]
-D = 0.4 * A
-N = [40, 60, 80, 100, 120, 160]
+B = [0, 0.1 * A, 0.5 * A]
+D = [0, 0.4 * A, 0.5 * A]
+N = [40, 60, 80, 100, 120, 140, 160]
 input = ["hamilt.dat", "rebde.dat", "reuna.dat", "reuns.dat"]
 
-# os.chdir("../../Scripts")
-i = 0
-j = 1
-diff = difference(A, B[1], D, N[i], N[j], input[0])
+# Input parameters
+b = 0
+d = 2
+i = 1       # first diagonalization basis size
+j = 2       # second diagonalization basis size
+
+# Enargy level difference at a change of basis
+diff = difference(A, B[b], D[d], N[i], N[j], input[0])
 stable_levels = np.where(np.abs(diff) < 5e-2)
 print(diff[np.abs(diff) < 5e-2].size,
-      " energy levels varry with less than 5% in a change of basis from N = ",
-      N[i], "to N = ", N[j])
+      " energy levels varry with less than 5% in a change of basis from N =",
+      N[i], "to N =", N[j])
 print("Stable levels: ", stable_levels)
-stats = statistics(A, B[1], D, N[i], input[0])
+# only 0 and 1 are valid for the last argument; can't separate reuna and reuns
+stats = statistics(A, B[b], D[d], N[i], 1)
 
+print("\nEnargy level difference at a change of basis: \n")
 plt.hist(diff, bins='auto')
 plt.show()
