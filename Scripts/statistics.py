34f87ff7f41aae82ed0492570ec5e5a249e6d4b7
sebastian <m.c.sebastian95@gmail.com>
2017-03-26 00:25:32 +0200

    Add statistics
    -Histograms for each representation
    -Histogram with the contributions summed from all representations

---
+++ Scripts/statistics.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+import select_rep
+from tools import cd
+from custom_parser import parse
+
+
+def relSpacing(rep):
+    deltaE = np.diff(rep)
+    avgSpacing = (rep[-1] - rep[0]) / rep.size
+    return deltaE / avgSpacing
+
+
+def histogram(data, name, use_sc, show, save=True):
+    plt.hist(data, bins=np.arange(0, 4, 1 / 4), label=name)
+    plt.legend()
+    if save:
+        plt.savefig(name + ('_sc.png' if use_sc else '.png'))
+    if show:
+        plt.show()
+    plt.clf()
+
+
+def main(b, d, n, use_sc, select=True, show=False):
+    if select:
+        select_rep.main(b, d, n, use_sc, stable_only=True)
+    reps = 'reuna', 'reuns', 'rebde'
+    cd(b, d, n)
+    rel_sp = []
+    w = []          # weights
+    for r in reps:
+        rep = np.loadtxt(r + '2' + ('_sc.dat' if use_sc else '.dat'),
+                         usecols=0)
+        rel_sp.append(relSpacing(rep))
+        w.append(np.ones(rel_sp[-1].shape) / 3)
+        histogram(rel_sp[-1], r, use_sc, show)
+
+    plt.hist(rel_sp, bins=np.arange(0, 4, 1 / 4), weights=w, stacked=True,
+             normed=True, label=reps)
+    plt.ylabel('P(S)')
+    plt.xlabel('S')
+    plt.legend()
+    plt.savefig('P(S)' + ('_sc.png' if use_sc else '.png'))
+    # plt.show()
+
+    os.chdir("../../Scripts")
+
+
+if __name__ == '__main__':
+    B, D, N, use_sc = parse()
+    for b in B:
+        for d in D:
+            for n in N:
+                main(b, d, n, use_sc)
