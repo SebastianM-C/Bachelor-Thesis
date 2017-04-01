fb9b7785f0b0964b9af85c2712887dcbd2d3369a
sebastian <m.c.sebastian95@gmail.com>
2017-04-01 19:11:53 +0300

    Formatting fixes
    -Fix approximation errors when importing
    -Use only stable levels
    -Add delta_n as parameter

--- Scripts/select_rep.py
+++ Scripts/select_rep.py
@@ -10,7 +10,7 @@ from custom_parser import parse
 from tools import cd
 
 
-def main(b, d, n, use_sc, stable_only=True):
+def main(b, d, n, use_sc, delta_n):
     print("Running with: B = " + str(b) + " D = " + str(d) + " N = " + str(n))
     cd(b, d, n)
     start = timer()
@@ -18,9 +18,8 @@ def main(b, d, n, use_sc, stable_only=True):
     ir_reps = eigensystem.levels(E, ket, use_sc)
 
     # Select only the stable levels
-    if stable_only:
-        stable_levels = diff.stable(E, ir_reps, b, d, n, use_sc)
-        E = E[:stable_levels]
+    stable_levels = diff.stable(E, ir_reps, b, d, n, use_sc, delta_n)
+    E = E[:stable_levels]
     stop = timer()
     print("get data: ", stop - start)
     rebde = open('rebde2' + ('_sc.dat' if use_sc else '.dat'), 'w')
@@ -34,15 +33,18 @@ def main(b, d, n, use_sc, stable_only=True):
         n2 = ket[i][1]
         if ir_reps[i] == 2:     # bidimensional representation (rebde)
             if not skip_next:
-                rebde.write(str(E[i]) + '\t' + str(n1) + '\t' + str(n2) + '\n')
+                rebde.write('{0:.18f}'.format(E[i]) + '\t' + str(n1) + '\t' +
+                            str(n2) + '\n')
                 skip_next = True
             else:
                 skip_next = False
         else:
             if n2 % 2:          # unidimensional anti-symmetric representation
-                reuna.write(str(E[i]) + '\t' + str(n1) + '\t' + str(n2) + '\n')
+                reuna.write('{0:.18f}'.format(E[i]) + '\t' + str(n1) + '\t' +
+                            str(n2) + '\n')
             else:               # unidimensional symmetric representation
-                reuns.write(str(E[i]) + '\t' + str(n1) + '\t' + str(n2) + '\n')
+                reuns.write('{0:.18f}'.format(E[i]) + '\t' + str(n1) + '\t' +
+                            str(n2) + '\n')
 
     rebde.close()
     reuna.close()
@@ -57,4 +59,4 @@ if __name__ == '__main__':
     for b in B:
         for d in D:
             for n in N:
-                main(b, d, n, use_sc)
+                main(b, d, n, use_sc, 20)
