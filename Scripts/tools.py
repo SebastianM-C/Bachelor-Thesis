255fa1b52ce1c565365435fa89f4812f9b528cb3
sebastian <m.c.sebastian95@gmail.com>
2017-04-17 18:13:14 +0300

    Improve directory changing logic

--- Scripts/tools.py
+++ Scripts/tools.py
@@ -4,6 +4,13 @@ import os
 import re
 
 
+def get_input():
+    """Return the input parameters (b, d, n) of the current path"""
+    regex = r"""B(0.[0-9])+ D(0.[0-9])+ N([0-9]+)"""
+    return [float(i) for i in
+            re.compile(regex).search(os.getcwd()).group(1, 2, 3)]
+
+
 def exists(b, d, n):
     """Check if the folder specified by the given parameters exists"""
     return os.path.isdir("../Output/B" +
@@ -29,26 +36,25 @@ def cd(b, d, n):
     and try again (maybe the last run had an error or
     the script did not reach the end)."""
 
+    # Check if already there
     try:
-        os.chdir("../Output/B" + str(b) + " D" + str(d) + " N" + str(n))
-    except FileNotFoundError as fnf:
-        try:
-            print(fnf.strerror + ':' + fnf.filename + '\nTrying again')
+        if [b, d, n] == get_input():
+            # print('Already there:', os.getcwd())
+            return
+        else:
             os.chdir("../../Scripts")
-            os.chdir("../Output/B" + str(b) +
-                     " D" + str(d) + " N" + str(n))
-            print('Succes\n' + 'Now in: ' + os.getcwd())
-        except FileNotFoundError:
-            print('Check the filename\n' + 'Now in: ' + os.getcwd())
+            # print('Went back to Scripts')
+    except AttributeError as e:
+        pass
+    # The script should be in the Scripts directory
+    if exists(b, d, n):
+        os.chdir('../Output/B' + str(b) + ' D' + str(d) + ' N' + str(n))
+        # print('Succes: ', os.getcwd())
+    else:
+        print('The specified directory does not exist!\n',
+              'Now in: ' + os.getcwd())
 
 
 def format_float(n):
     """Converts the number to int if possible"""
     return ('{:n}' if n == int(n) else '{:.8g}').format(n)
-
-
-def get_input():
-    """Return the input parameters (b, d, n) of the current path"""
-    regex = r"""B(0.[0-9])+ D(0.[0-9])+ N([0-9]+)"""
-    return [float(i) for i in
-            re.compile(regex).search(os.getcwd()).group(1, 2, 3)]
